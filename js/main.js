49d0c26838bda28d48399823216d146ead9c836c
Jimmy Ho <jim.eho97@gmail.com>
2020-04-23 17:20:59 -0700

    static sidebar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //Controls Navbar Hide
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
+window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
         document.getElementById("navbar").style.top = "0";
@@ -10,3 +10,22 @@ window.onscroll = function () {
     prevScrollpos = currentScrollPos;
 }
 
+// Scroll to specific values
+// scrollTo is the same
+window.scroll({
+    top: 2500,
+    left: 0,
+    behavior: 'smooth'
+});
+
+// Scroll certain amounts from current position 
+window.scrollBy({
+    top: 100, // could be negative value
+    left: 0,
+    behavior: 'smooth'
+});
+
+// Scroll to a certain element
+document.querySelector('.hello').scrollIntoView({
+    behavior: 'smooth'
+});
